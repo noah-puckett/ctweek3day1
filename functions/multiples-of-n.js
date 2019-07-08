e30c4d3eda7c3059063c23742b0aac62a18c33d8
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-07-08 15:57:32 -0700

    aded test and function, no test yet

---
+++ functions/multiples-of-n.js
@@ -0,0 +1,8 @@
+function multiplesOfN(int) {
+
+    
+
+    //return array that has multiples of that number, somehow
+}
+
+module.exports = { multiplesOfN };
---
+++ test/multiples-of-n.test.js
@@ -0,0 +1,9 @@
+const { multiplesOfN } = require('../functions/multiples-of-n');
+
+describe('MATH', () => {
+
+    it('multiples of n, whatever that means', () => {
+        
+    });
+
+});
